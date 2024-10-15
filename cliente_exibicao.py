67bdb602c54275a2006cfaf0456ce57a90bc4e30
mathkzm <matheusmiyashiro.mm@gmail.com>
2024-10-15 08:30:21 -0400

    Implementação das funções de exibição das mensagens e da criação da mensagem OI

--- cliente_exibicao.py
+++ cliente_exibicao.py
@@ -10,19 +10,28 @@ import threading
 def receber_msgs(sock):
     while True:
         msg, _ = sock.recvfrom(1024)
-        exibir_mensagem(msg)
+        exibir_msg(msg)
 
 # Função de exibir mensagens.
 # Decodifica a mensagem em tipo, remetente, destinatário e o texto.
 # Verifica se o destinatário é 0 (exibir a todos). Nesse caso, especificar no print que é uma msg pública.
 # Se o destinatário for diferente de 0, especificar que a msg é privada e colocar o destinatário.
-def exibir_mensagem(mensagem):
+def exibir_msg(msg):
+    tipo_msg, id_remetente, id_destino, texto = decodificar_msg(msg)
+
+    if id_destino == 0:
+        print(f"[Todos] from {id_remetente}: {texto}")
+    else:
+        print(f"[Privado] from {id_remetente} to {id_destino}: {texto}")
 
 # Cria o envio da mensagem OI
-def criar_mensagem_oi(client_id, username):
+def criar_msg_oi(id_cliente, nome_usuario):
+    tipo = 0
+    return f"{tipo} {id_cliente} {nome_usuario}".encode()
+
 
 # Elaborar a função da decodificação de mensagens nos campos especificados (ID, remetente, destinatário, texto)
-def decodificar_mensagem(mensagem):
+def decodificar_msg(msg):
 
 # Estabelecer a execução exatamente como o especificado no trabalho.
 # python cliente_exibicao.py <ID> <nome_usuario> <endereço_servidor:porta
